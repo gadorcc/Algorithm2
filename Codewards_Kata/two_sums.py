35f04e67024d12b7bd82744dd9b20c80ecbdacf4
Gador Cruz <gadorcruz@hotmail.com>
2023-04-09 12:07:43 +0100

    done two sums

--- Codewards_Kata/two_sums.py
+++ Codewards_Kata/two_sums.py
@@ -1,6 +1,44 @@
 def two_sum(numbers, target):
-	return []
+    lst = list(combinations(numbers,2))
+    for tuple in lst:
+        result = sum(tuple)
+        if result == target:
+            list_win = list(tuple)
+    l = []
+    x = range(len(numbers))
+    for n in x:
+        for num in list_win:
+            if numbers[n] == num:
+                l.append(n)
+    mylist = list(set(l))
+    return mylist
 
-# test.assert_equals(sorted(two_sum([1,2,3], 4)), [0,2])
+
+# x = range(6)
+# for n in x:
+#   print(n)
+
 # test.assert_equals(sorted(two_sum([1234,5678,9012], 14690)), [1,2])
-# test.assert_equals(sorted(two_sum([2,2,3], 4)), [0,1])
+
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+
+two_sum([1234,5678,9012], 14690)
