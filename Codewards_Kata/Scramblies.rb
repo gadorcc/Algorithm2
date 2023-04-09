f30c00aef33cb50b0bafcc370933ed95e2d482e5
Gador Cruz <gadorcruz@hotmail.com>
2023-04-09 08:41:24 +0100

    two sums kata

--- Codewards_Kata/Scramblies.rb
+++ Codewards_Kata/Scramblies.rb
@@ -1,19 +1,27 @@
+# def scramble(s1,s2)
+#   a2 = s2.chars
+#   a1 = s1.chars
+#   n = 0
+#   a2.each do |char|
+#     if s1.include?(char)
+#       p a1.delete(char)
+#       n += 1
+#     end
+
+#   end
+#   if n == s2.length
+#     return true
+#   else
+#     return false
+#   end
+# end
+
 def scramble(s1,s2)
   a2 = s2.chars
   a1 = s1.chars
-  n = 0
-  a2.each do |char|
-    if s1.include?(char)
-      p a1.delete(char)
-      n += 1
-    end
-  end
-  if n == s2.length
-    return true
-  else
-    return false
-  end
+  p h2 = Hash.new(0).tap { |h| a2.each { |char| h[char] += 1 } }
+  p h1 = Hash.new(0).tap { |h| a1.each { |char| h[char] += 1 } }
 end
-
-
-scramble('katas',             'steak'   )
+s1 = "abcdefghijklmnopqrstuvwxyz" * 100_000
+s2 = "zyxcba" * 90_000
+scramble('cedewaraaossoqqyt', 'codewars'  )
