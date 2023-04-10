1b592efaea35eb36aeed15735527cb2b8cadaf43
Gador Cruz <gadorcruz@hotmail.com>
2023-04-10 11:30:30 +0100

    katas

---
+++ Codewards_Kata/Greatest_position.py
@@ -0,0 +1,11 @@
+from collections import Counter
+def greatest_distance(arr):
+    for idx, x in enumerate(arr):
+        print(x,idx)
+
+
+
+
+
+
+greatest_distance([0,7,0,2,3,7,0,-1,-2])
---
+++ Codewards_Kata/sum_even_numbers.py
@@ -0,0 +1,8 @@
+def sum_even_numbers(seq):
+    even_list = [num for num in seq if num % 2 == 0]
+    return (sum(even_list))
+
+
+
+
+sum_even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
