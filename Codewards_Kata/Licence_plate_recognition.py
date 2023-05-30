379b38538030f50ff87070b26769a159d160fce8
Gador Cruz <gadorcruz@hotmail.com>
2023-05-30 09:53:13 +0100

    add licence recognition kata

---
+++ Codewards_Kata/Licence_plate_recognition.py
@@ -0,0 +1,14 @@
+def recognize(s):
+    s_list = [char for char in s]
+    for count, value in enumerate(s_list):
+        print(count,value)
+
+
+
+recognize(
+        ' _  _     _  _  _  _  _ \n'
+        ' _| _||_||_ |_   ||_||_|\n'
+        '|_  _|  | _||_|  ||_| _|',
+
+
+    )
