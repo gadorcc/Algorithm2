9768fd94cdf3507cab9864c1a28ee9c93e9e552f
Gador Cruz <gadorcruz@hotmail.com>
2023-06-08 10:24:19 +0100

    add ruby kata sort the odd

--- Chapter01_Arrays&Strings/Check_permutation.rb
+++
@@ -1,5 +0,0 @@
-def permutation?(str_1, str_2)
-  str_1.chars.sort == str_2.chars.sort
-end
-
-p permutation?("abcd","dabc")
--- Chapter01_Arrays&Strings/Palindrome_permutation.rb
+++
@@ -1,7 +0,0 @@
-def palindrome(string, palindrome)
-
-end
-
-palindrome("Tact Coa", "taco cat")
-tacocat
-aoctcat
